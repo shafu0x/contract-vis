7d35954aec317ebc1fa8dc089de28d6c254cc09f
shafu.eth <selfouly@gmail.com>
2023-10-24 00:04:28 +0300

    refactor

--- 3d.py
+++ 3d.py
@@ -4,48 +4,39 @@ import os
 import sys
 
 if len(sys.argv) != 2:
-    print("Usage: python run.py [path to dir]")
+    print("Usage: python run.py [path to file]")
     exit(1)
 else:
-    PATH = sys.argv[1]
+    file = sys.argv[1]
 
-files = os.listdir(PATH)
+with open(file, "r") as f:
+    bytecode = f.read()
 
-bitmaps = []
+if bytecode[:2] == "0x":
+    bytecode = bytecode[2:] 
 
-for file in files:
-    path = PATH + file
+bytecode_list = [bytecode[i:i+2] for i in range(0, len(bytecode), 2)]
+bytecode_list = bytecode_list[:-1] # remove \n
 
-    with open(path, "r") as f:
-        bytecode = f.read()
+bytecode_pairs = []
+for i in range(len(bytecode_list)-2):
+    x = int(bytecode_list[i], 16)
+    y = int(bytecode_list[i+1], 16)
+    z = int(bytecode_list[i+2], 16)
+    bytecode_pairs.append((x, y, z))
 
-    if bytecode[:2] == "0x":
-        bytecode = bytecode[2:] 
+bitmap = np.zeros((256, 256, 256), dtype=np.uint8)
+for pair in bytecode_pairs:
+    bitmap[pair[0], pair[1], pair[2]] = 255
 
-    bytecode_list = [bytecode[i:i+2] for i in range(0, len(bytecode), 2)]
-    bytecode_list = bytecode_list[:-1] # remove \n
-
-    bytecode_pairs = []
-    for i in range(len(bytecode_list)-2):
-        x = int(bytecode_list[i], 16)
-        y = int(bytecode_list[i+1], 16)
-        z = int(bytecode_list[i+2], 16)
-        bytecode_pairs.append((x, y, z))
-
-    bitmap = np.zeros((256, 256, 256), dtype=np.uint8)
-    for pair in bytecode_pairs:
-        bitmap[pair[0], pair[1], pair[2]] = 255
-
-    bitmaps.append(bitmap)
-
-files = [files[i].split(".")[0] for i in range(6)] # remove extensions
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-for i in range(6):
-    x, y, z = bitmaps[i].nonzero()
-    ax.scatter(x, y, z, marker=".", label=files[i])
+x = bitmap.nonzero()[0]
+y = bitmap.nonzero()[1]
+z = bitmap.nonzero()[2]
+ax.scatter(x, y, z, marker=".", label=file.split("/")[-1].split(".")[0])
 
 ax.set_xlabel("x")
 ax.set_ylabel("y")
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 python3 2d.py /Users/shafu/contract-vis/examples/
-python3 3d.py /Users/shafu/contract-vis/examples/
+python3 3d.py /Users/shafu/contract-vis/examples/ens.bytecode
