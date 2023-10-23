65502c385968384fb36b5abce9e6e529d6bffd6c
shafu.eth <selfouly@gmail.com>
2023-10-23 23:37:30 +0300

    refactor

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-PATH = "/Users/shafu/contract-vis/examples"
+PATH = "/Users/shafu/contract-vis/examples/"
 files = os.listdir(PATH)
 
 bitmaps = []
@@ -13,14 +13,17 @@ for file in files:
     with open(path, "r") as f:
         bytecode = f.read()
 
-    bytecode = bytecode[2:]
+    if bytecode[:2] == "0x":
+        bytecode = bytecode[2:] 
 
     bytecode_list = [bytecode[i:i+2] for i in range(0, len(bytecode), 2)]
-    bytecode_list = bytecode_list[:-1]
+    bytecode_list = bytecode_list[:-1] # remove \n
 
     bytecode_pairs = []
     for i in range(len(bytecode_list)-1):
-        bytecode_pairs.append((int(bytecode_list[i], 16), int(bytecode_list[i+1], 16)))
+        x = int(bytecode_list[i], 16)
+        y = int(bytecode_list[i+1], 16)
+        bytecode_pairs.append((x, y))
 
     bitmap = np.zeros((256, 256), dtype=np.uint8)
     for pair in bytecode_pairs:
@@ -30,8 +33,7 @@ for file in files:
 
 fig, axes = plt.subplots(2, 3)
 
-bitmaps = [bitmaps[i] for i in range(6)]
-files = [files[i].split(".")[0] for i in range(6)]
+files = [files[i].split(".")[0] for i in range(6)] # remove extensions
 
 for i in range(2):
     for j in range(3):
